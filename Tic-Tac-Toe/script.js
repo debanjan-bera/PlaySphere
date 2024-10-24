ed1a441fbe744ba6ba0bcda5d6b152e530c989bc
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-10-24 10:11:40 +0530

    Modify code

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -13,9 +13,9 @@ let xTurn = true,
   drawMatch = 0,
   xScoreCount = 0,
   oScoreCount = 0,
-  roundCount = 0;
-  win = false;
-  // count = 0;
+  roundCount = 1;
+win = false;
+// count = 0;
 
 const winningPatterns = [
   [0, 1, 2],
@@ -28,8 +28,25 @@ const winningPatterns = [
   [6, 7, 8],
 ];
 
+btnRef.forEach((ele, index) => {
+  ele.addEventListener("click", () => {
+    if (ele.innerText === "" && !win) {
+      changeTurn(ele);
+      console.log(ele.innerText);
+      storeArr[index] = ele.innerText;
+      win = winChecker();
+      sliderGretting(win,ele);
+    }
+  });
+});
+
+ContinueGame(newGameBtn);
+resetBtnFun(resetBtn);
+
 function changeTurn(ele) {
-  ele.innerText = xTurn ?  ele.style.color = "#0068a5": ele.style.color = "rgb(255 153 0)";
+  ele.innerText = xTurn
+    ? (ele.style.color = "#0068a5")
+    : (ele.style.color = "rgb(255 153 0)");
   ele.innerText = xTurn ? "X" : "O";
   xTurn = !xTurn;
   turnSlider.classList.toggle("slider-X");
@@ -44,7 +61,7 @@ function winChecker() {
       return true; // A win has occurred
     }
   }
-  
+
   return false; // No win found
 }
 
@@ -53,18 +70,22 @@ function highlightWinningCombination(pattern) {
     btnRef[index].style.backgroundColor = "#ff4b4b";
   });
   msRef.innerText = `Winner is ${btnRef[pattern[0]].innerText}`;
-  changeSlider.innerHTML = `<div class="winning-part">Winner is ${
-    btnRef[pattern[0]].innerText
-  }</div>`;
-
-  changeSlider.style.backgroundColor = " rgb(114, 255, 114)";
   popUpMass.classList.toggle("hide");
 }
 
+
+function popUpMess() {
+  if ((roundCount = 2)) {
+    msRef.innerText = `Winner is ${btnRef[pattern[0]].innerText}`;
+    popUpMass.classList.toggle("hide");
+  }
+}//under construction
+
+
+
 function updateScore(winner) {
   roundCount++;
-  
-  document.querySelector(".round-mes-scroe").innerText=roundCount;
+  document.querySelector(".round-mes-scroe").innerText = roundCount;
   if (winner === "X") {
     isX.innerText = ++xScoreCount;
   } else if (winner === "O") {
@@ -72,62 +93,55 @@ function updateScore(winner) {
   }
 }
 
-btnRef.forEach((ele, index) => {
-  ele.addEventListener("click", () => {
-    if (ele.innerText === "" && !win) {
-      changeTurn(ele);
-      console.log(ele.innerText);
-      storeArr[index] = ele.innerText;
-      win = winChecker();
-      if (!win) {
-        drawMatch++;
-        if (drawMatch >= 9) {
-          msRef.innerText = "Draw";
-          popUpMass.classList.remove("hide");
-          changeSlider.innerHTML = `<div class="winning-part">Draw</div>`;
-        }
-      }
+
+function sliderGretting(win,ele){
+  if(win){
+    changeSlider.innerHTML = `<div class="winning-part">Winner is ${
+      ele.innerText
+    }</div>`;
+    changeSlider.style.backgroundColor = "rgb(114, 255, 114)";
+  }else{
+    drawMatch++;
+    if (drawMatch >= 9) {
+      msRef.innerText = "Draw";
+      popUpMass.classList.remove("hide");
+      changeSlider.innerHTML = `<div class="winning-part">Draw</div>`;
     }
+  }
+}//under construction
+
+
+
+
+function ContinueGame(ele) {
+  ele.addEventListener("click", () => {
+    btnRef.forEach((e) => {
+      e.innerText = "";
+      e.style.backgroundColor = "";
+    });
+    popUpMass.classList.toggle("hide");
+    win = false;
+    xTurn = true;
+    drawMatch = 0;
+    storeArr.fill("");
+    changeSlider.innerHTML = `<div class="text">X</div>
+              <div class="text">O</div>
+              <div class="slider slider-X slider-O"></div>`;
+    changeSlider.style.backgroundColor = " rgba(255, 255, 255, 0.318)";
+    turnSlider.classList.add("slider-X");
+    document.querySelector(".round-scroe").innerText = roundCount;
   });
-});
-
-newGameBtn.addEventListener("click", () => {
-  btnRef.forEach((ele) => {
-    ele.innerText = "";
-    ele.style.backgroundColor = "";
-  });
-  
-  ContinueGame();
-});
-
-function ContinueGame() {
-  popUpMass.classList.toggle("hide");
-  win = false;
-  xTurn = true;
-  drawMatch = 0;
-  storeArr.fill("");
-  changeSlider.innerHTML = `<div class="text">X</div>
-            <div class="text">O</div>
-            <div class="slider slider-X slider-O"></div>`;
-  changeSlider.style.backgroundColor = " rgba(255, 255, 255, 0.318)";
-  turnSlider.classList.add("slider-X");
-  document.querySelector(".round-scroe").innerText=roundCount;
-
 }
-
-// btnRef.forEach((e,index)=>{
-//   // storeArr.fill("");
-//   e.innerText = storeArr[index]
-//   console.log(index);
-// })
-resetBtn.addEventListener("click", () => {
-  btnRef.forEach((ele) => {
-    ele.innerText = "";
+function resetBtnFun(ele) {
+  ele.addEventListener("click", () => {
+    btnRef.forEach((e) => {
+      e.innerText = "";
+    });
+    win = false;
+    xTurn = true;
+    drawMatch = 0;
+    storeArr.fill("");
+    turnSlider.classList.add("slider-X");
+    xScoreCount = oScoreCount = isO.innerText = isX.innerText = 0;
   });
-  win = false;
-  xTurn = true;
-  drawMatch = 0;
-  storeArr.fill("");
-  turnSlider.classList.add("slider-X");
-  xScoreCount = oScoreCount = isO.innerText = isX.innerText = 0;
-});
+}
