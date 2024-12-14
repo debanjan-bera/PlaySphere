fa8541e1f33b6da478bb23111ec7fdb6718565bf
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-12-14 21:41:31 +0530

    .

--- playsphere/src/component/tictactoe/GameZone.jsx
+++ playsphere/src/component/tictactoe/GameZone.jsx
@@ -2,35 +2,12 @@
 // import BoxComponent from "./box.jsx";
 import "./game.css";
 
-// const MainGameFun = () => {
-//   const handelClick = () => {
-//     console.log("hello");
-//   };
-
-//   return (
-//     <div id="cont" className="contBox grid gap-1 grid-cols-3 grid-rows-3 rounded-md overflow-hidden"
-//     >
-
-//       <BoxComponent />
-//       <div
-//         className="bg-black/30 backdrop-blur-xl flex items-center justify-center rounded-md border-4 border-solid border-blue-700"
-//         onClick={() => handelClick()}
-//       ></div>
-//     </div>
-//   );
-// };
-
-// export default MainGameFun;
-// import React from 'react';
 import BoxComponent from './box';
 
 const MainGameFun = () => {
   let i = 'X'
   let cross = false
-  const handleEvent = (event) => {
-    console.log('Event triggered:', event);
-    console.log('Event type:', event.type); // e.g., 'click'
-    // console.log(i,cross);
+  const handleEvent = () => {
     hello()
 
   };
@@ -38,15 +15,14 @@ const MainGameFun = () => {
     i = cross ? "X" : "O"
     cross = !cross 
     console.log(i,cross);
-    return <h2>{i}</h2>
-
+    return i
 }
   return (
     <div id="cont" className="contBox grid gap-1 grid-cols-3 grid-rows-3 rounded-md overflow-hidden">
       
-      <BoxComponent onEvent={handleEvent} valuuofValue={<h2>{i}</h2>}/>
-      <BoxComponent onEvent={handleEvent} valuuofValue={<h2>{i}</h2>}/>
-      <BoxComponent onEvent={handleEvent} valuuofValue={<h2>{i}</h2>}/>
+      <BoxComponent onEvent={handleEvent} valuuofValue={{i}}/>
+      <BoxComponent onEvent={handleEvent} valuuofValue={{i}}/>
+      <BoxComponent onEvent={handleEvent} valuuofValue={{i}}/>
 
     </div>
   );
--- playsphere/src/component/tictactoe/box.jsx
+++ playsphere/src/component/tictactoe/box.jsx
@@ -1,14 +1,21 @@
 
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const BoxComponent = ({ onEvent,helloFun}) => {
-    
+
+
+const BoxComponent = ({ onEvent,valuOfBlock}) => {
+  const [value,setValue ] = useState('X')
+  setValue(()=>{
+    value = valuOfBlock
+  })
+    console.log(valuOfBlock);
   return (
     <>
     <button className="bg-black/30 backdrop-blur-xl flex items-center justify-center rounded-md border-4 border-solid border-blue-700" 
     onClick={onEvent}
     >
-        {helloFun}
+        {value}
     </button>
 
     </>
