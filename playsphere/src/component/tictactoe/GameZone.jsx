a4c18e5a3643b846bbe799524744357e44913206
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-12-19 18:04:26 +0530

    Add winner detection logic to Tic Tac Toe game

--- playsphere/src/component/tictactoe/GameZone.jsx
+++ playsphere/src/component/tictactoe/GameZone.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./game.css";
+import { isMatchWinner } from "./LogialComponent";
 export const MainGameFun = () => {
   const [gameArr, setGameArr] = useState(Array(9).fill({ value: "", color: "", disabled: false }));
   const [isValue, setValue] = useState(false);
+  
   const handleGameClick = (index) => {
     if (gameArr[index].disabled) return;
     const newGameArr = [...gameArr];
@@ -10,8 +12,10 @@ export const MainGameFun = () => {
     const newColor = isValue ? "text-pink-300" : "text-blue-300";
     newGameArr[index] = { value: newValue, color: newColor, disabled: true };
     setGameArr(newGameArr);
+    isMatchWinner(newGameArr,setGameArr);
     setValue(!isValue);
   };
+
   return (
     <>
       <div id="cont" className="contBox grid grid-cols-3 grid-rows-3 gap-3 justify-center items-center text-7xl font-semibold">
---
+++ playsphere/src/component/tictactoe/LogialComponent.jsx
@@ -0,0 +1,37 @@
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+export const isMatchWinner = (gameArr,setGameArr)=>{
+  const emptyCount = gameArr.filter((element) => element.value !== "").length;
+  if(emptyCount>4){
+    winChecker(gameArr,setGameArr)
+  }
+}
+const winChecker = (gameArr,setGameArr) => {
+    for (const pattern of winningPatterns) {
+      const [a, b, c] = pattern;
+      if (gameArr[a].value && gameArr[a].value === gameArr[b].value &&
+        gameArr[a].value === gameArr[c].value) {
+        console.log(`Winner: ${gameArr[a].value}`);
+        console.log(gameArr[a].value);
+        resetDisabled(gameArr,setGameArr)
+        return gameArr[a].value;
+      }
+    }
+    return null
+};
+
+const resetDisabled = (gameArr,setGameArr) => {
+  const updatedGameArr = gameArr.map((element) => ({
+    ...element, 
+    disabled: true,
+  }));
+  setGameArr(updatedGameArr)
+};
