462b82a7f1f83c4ea09908c88dc0140968308977
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-12-20 18:57:33 +0530

    .

--- playsphere/src/component/tictactoe/GameZone.jsx
+++ playsphere/src/component/tictactoe/GameZone.jsx
@@ -4,7 +4,6 @@ import { isMatchWinner } from "./LogialComponent";
 export const MainGameFun = () => {
   const [gameArr, setGameArr] = useState(Array(9).fill({ value: "", color: "", disabled: false }));
   const [isValue, setValue] = useState(false);
-  
   const handleGameClick = (index) => {
     if (gameArr[index].disabled) return;
     const newGameArr = [...gameArr];
@@ -15,23 +14,20 @@ export const MainGameFun = () => {
     isMatchWinner(newGameArr,setGameArr);
     setValue(!isValue);
   };
-
+  const handleGameReset = ()=>{
+    setGameArr(Array(9).fill({ value: "", color: "", disabled: false }))
+    setValue(false);
+  }
   return (
     <>
-      <div id="cont" className="contBox grid grid-cols-3 grid-rows-3 gap-3 justify-center items-center text-7xl font-semibold">
+      <div id="cont" className="contBox h-[96%] w-[26rem] grid grid-cols-3 grid-rows-3 gap-3 justify-center items-center text-7xl font-semibold">
         {gameArr.map((value, index) => (
-          <button
-            className={`gameBox ${value.color}`}
-            key={index}
-            onClick={() => handleGameClick(index)}
-          >
+          <button className={`gameBox h-full w-full ${value.color}`} key={index} onClick={() => handleGameClick(index)}>
             {value.value}
           </button>
         ))}
       </div>
-
+      <button onClick={handleGameReset}>Reset</button>
     </>
   );
 };
-
-// export default MainGameFun;
--- playsphere/src/component/tictactoe/LogialComponent.jsx
+++ playsphere/src/component/tictactoe/LogialComponent.jsx
@@ -10,15 +10,12 @@ const winningPatterns = [
 ];
 export const isMatchWinner = (gameArr,setGameArr)=>{
   const emptyCount = gameArr.filter((element) => element.value !== "").length;
-  if(emptyCount>4){
-    winChecker(gameArr,setGameArr)
-  }
+  if(emptyCount>4) winChecker(gameArr,setGameArr)
 }
 const winChecker = (gameArr,setGameArr) => {
     for (const pattern of winningPatterns) {
       const [a, b, c] = pattern;
-      if (gameArr[a].value && gameArr[a].value === gameArr[b].value &&
-        gameArr[a].value === gameArr[c].value) {
+      if (gameArr[a].value && gameArr[a].value === gameArr[b].value && gameArr[a].value === gameArr[c].value) {
         console.log(`Winner: ${gameArr[a].value}`);
         console.log(gameArr[a].value);
         resetDisabled(gameArr,setGameArr)
